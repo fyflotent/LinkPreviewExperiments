c0cd42b796aa22ff3258e4f16708d8beb23391f5
Chris Rice <chris.189.r@gmail.com>
2024-10-01 18:17:12 -0400

    Add open graph scraping api

--- back-end/src/api/preview.ts
+++ back-end/src/api/preview.ts
@@ -1,21 +1,35 @@
 import express from "express";
+import ogs from "open-graph-scraper";
 
 const router = express.Router();
 
-type UrlPreviewResponse = {
-  title: string;
-  description: string;
-  image: string;
-};
+type UrlPreviewResponse =
+  | {
+      title: string | undefined;
+      description: string | undefined;
+      image: string | undefined;
+    }
+  | { error: string };
 
 router.post<{ url: string }, UrlPreviewResponse>("/", (req, res) => {
   const url = req.body.url;
-  console.log(url);
-  res.json({
-    title: "Title",
-    description: "Description",
-    image: "https://via.placeholder.com/150",
-  });
+  console.debug("Fetching URL preview for", url);
+  ogs({ url: url })
+    .then((data) => {
+      const urlHost = new URL(url);
+
+      const imgUrl = data.result.ogImage?.[0]?.url;
+
+      res.json({
+        title: data.result.ogTitle,
+        description: data.result.ogDescription,
+        image: imgUrl?.startsWith("/") ? `${urlHost.origin}${imgUrl}` : imgUrl,
+      });
+    })
+    .catch((e) => {
+      console.error("Error fetching URL preview", e);
+      res.status(400).json({ error: "Error fetching URL preview" });
+    });
 });
 
 export default router;
